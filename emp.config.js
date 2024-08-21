e260ba348e638e4aaea6d0c56db91e203fa07c8c
xuhongbin <xuhongbin@yy.com>
2024-08-21 17:09:03 +0800

    feat: up

--- emp.config.js
+++ emp.config.js
@@ -1,34 +1,34 @@
-import path from 'path'
-import {defineConfig} from '@empjs/cli'
-import pluginReact from '@empjs/plugin-react'
+import path from "path";
+import { defineConfig } from "@empjs/cli";
+import pluginReact from "@empjs/plugin-react";
 
-export default defineConfig(store => {
-  const appSrc = store.resolve('src')
-  console.log(appSrc)
-  return {
-    plugins: [pluginReact()],
-    define: {
-      PUBLIC_URL: '.',
-    },
-    html: {
-      template: store.resolve('template/index.html'),
-      favicon: store.resolve('template/favicon.png'),
-    },
-    build: {
-      staticDir: 'static',
-    },
-    resolve: {
-      alias: {
-        page: path.join(appSrc, 'page'),
-        assets: path.join(appSrc, 'assets'),
-        components: path.join(appSrc, 'components'),
-        utils: path.join(appSrc, 'utils'),
-        contexts: path.join(appSrc, 'contexts'),
-        api: path.join(appSrc, 'api'),
-      },
-    },
-    debug: {
-      // showRsconfig: true,
-    },
-  }
-})
+export default defineConfig((store) => {
+	const appSrc = store.resolve("src");
+	console.log(appSrc);
+	return {
+		plugins: [pluginReact()],
+		define: {
+			PUBLIC_URL: ".",
+		},
+		html: {
+			template: store.resolve("src/index.html"),
+			favicon: store.resolve("src/favicon.png"),
+		},
+		build: {
+			staticDir: "static",
+		},
+		resolve: {
+			alias: {
+				page: path.join(appSrc, "page"),
+				assets: path.join(appSrc, "assets"),
+				components: path.join(appSrc, "components"),
+				utils: path.join(appSrc, "utils"),
+				contexts: path.join(appSrc, "contexts"),
+				api: path.join(appSrc, "api"),
+			},
+		},
+		debug: {
+			// showRsconfig: true,
+		},
+	};
+});
