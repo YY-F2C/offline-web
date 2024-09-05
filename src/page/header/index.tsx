1016e56c49855711a404cc8aea4f8e18a0026f54
Howie <593766927@qq.com>
2024-09-05 19:43:46 +0800

    feat：删除 entry 无用wenjian

--- src/page/header/index.tsx
+++ src/page/header/index.tsx
@@ -1,8 +1,8 @@
-import {ChevronLeft, Settings} from 'react-feather'
 import React, { memo, useMemo, Fragment } from "react"
+import {ChevronLeft, Settings} from 'react-feather'
 import { useTranslation } from 'react-i18next';
 
-import LangSetting from 'page/entry/LangSetting';
+import LangSetting from 'components/LangSetting';
 
 import Overlay from './Overlay'
 import SettingsPanel from './Settings'
@@ -19,32 +19,17 @@ const Header = (props) => {
         onBack,
     } = props;
 
-    const { t } = useTranslation(); 
+    const { t } = useTranslation('header'); 
     
     const hasNames = useMemo(() => {
         return !!(pageName && frameName);
     }, [pageName, frameName]);
-
-    const logoVisible = mode === 'local' ? true : !hasNames;
 
     return (
         <header className="app-header">
             <a className="header-logo" rel="noopener noreferrer" onClick={onBack}>
                 <img src="https://med-fe.cdn.bcebos.com/f2c_offline/Frame%20298.png" alt="" />
             </a>
-            {/* {logoVisible 
-                ? (
-                    <a className="header-logo" rel="noopener noreferrer">
-                        <img src="https://med-fe.cdn.bcebos.com/f2c_offline/Frame%20298.png" alt="" />
-                    </a>
-                ) 
-                : (
-                    <span className="header-back" onClick={onBack}>
-                        <ChevronLeft size={24} />
-                    </span>
-                )
-            } */}
-            {/* <span className="header-filename">{documentName}</span> */}
             <span className="header-space" />
             {hasNames 
                 ? (
